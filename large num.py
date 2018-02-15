5ab5e19e50fcd0d488c25224a3c77632f21bdfae
gs28 <36412873+gs28@users.noreply.github.com>
2018-02-14 21:13:45 -0800

    Create large num.py

---
+++ large num.py
@@ -0,0 +1,20 @@
+num1 = int(input("Enter a num1: "))
+
+num2 = int(input("Enter a num2: "))
+
+num3 = int(input("Enter a num3: "))
+
+if (num1 >= num2) and (num1 >= num3):
+   
+largest = num1
+
+elif 
+(num2 >= num1) and (num2 >= num3):
+   
+largest = num2
+
+else:
+   
+largest = num3
+
+print("The largest number between",num1,",",num2,"and",num3,"is",largest) 
