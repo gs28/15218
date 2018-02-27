2dd602e427ed176e70f12c16fe0d6f7e3047cc86
gs28 <36412873+gs28@users.noreply.github.com>
2018-02-27 03:50:35 -0800

    Update swap odd even.py

--- swap odd even.py
+++ swap odd even.py
@@ -1,8 +1,2 @@
-def swapBits(x) :
-  even_bits = x & 0xAAAAAAAA
-  odd_bits = x & 0x55555555
-  even_bits >>= 1
-  odd_bits <<= 1
-  return (even_bits | odd_bits)
-x = 23
-    print(swapBits(x))
+name = 'guvi'
+print(''.join([c[1] + c[0] for c in zip(name[::2], name[1::2])]))
